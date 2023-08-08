12f392e33504b5f5acd222721c8ecd43ef39d0f1
irwanto <yantwant@gmail.com>
2023-08-08 13:05:33 +0700

    Add files via upload

---
+++ js/diagramanim.js
@@ -0,0 +1,257 @@
+
+let icmanufacturing = gsap.to("#icmanufacturing", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let manufacturing = gsap.to("#manufacturing", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icconstruction = gsap.to("#icconstruction", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let construction = gsap.to("#construction", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icwholesale = gsap.to("#icwholesale", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let wholesale = gsap.to("#wholesale", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icbankinginsurance = gsap.to("#icbankinginsurance", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let banking = gsap.to("#banking", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icphone = gsap.to("#icphone", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let telecomuni = gsap.to("#telecomunications", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let ichealthcare = gsap.to("#ichealthcare", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let healthcare = gsap.to("#healthcare", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icsaving = gsap.to("#icsaving", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let financial = gsap.to("#financial", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let ictower = gsap.to("#ictower", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let telecom = gsap.to("#telecom", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let iccare = gsap.to("#iccare", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let hospital = gsap.to("#hospital", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icgoodybag = gsap.to("#icgoodybag", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let consumer = gsap.to("#consumer", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let ichouse = gsap.to("#ichouse", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let housing = gsap.to("#housing", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icgraduate = gsap.to("#icgraduate", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let education = gsap.to("#education", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icbusiness = gsap.to("#icbusiness", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let btoc = gsap.to("#btoc", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icbtob = gsap.to("#icbtob", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let btob = gsap.to("#btob", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icagriculture = gsap.to("#icagriculture", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let agriculture = gsap.to("#agriculture", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icgear = gsap.to("#icgear", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let resources = gsap.to("#resources", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let icfood = gsap.to("#icfood", {scale: 1.5,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+let beverages = gsap.to("#foodbeverages", {scale: 1.8,transformOrigin: "center center",ease: "power3.inOut"})
+  .reversed(true);
+
+document.querySelector("#icmanufacturing").addEventListener("mouseenter", doIcManufacturing);
+document.querySelector("#icmanufacturing").addEventListener("mouseleave", doIcManufacturing);
+
+document.querySelector("#icconstruction").addEventListener("mouseenter", doIcConstruction);
+document.querySelector("#icconstruction").addEventListener("mouseleave", doIcConstruction);
+
+document.querySelector("#icwholesale").addEventListener("mouseenter", doIcWholesale);
+document.querySelector("#icwholesale").addEventListener("mouseleave", doIcWholesale);
+
+document.querySelector("#icbankinginsurance").addEventListener("mouseenter", doIcBanking);
+document.querySelector("#icbankinginsurance").addEventListener("mouseleave", doIcBanking);
+
+document.querySelector("#icphone").addEventListener("mouseenter", doIcPhone);
+document.querySelector("#icphone").addEventListener("mouseleave", doIcPhone);
+
+document.querySelector("#ichealthcare").addEventListener("mouseenter", doIcHealthcare);
+document.querySelector("#ichealthcare").addEventListener("mouseleave", doIcHealthcare);
+
+document.querySelector("#icsaving").addEventListener("mouseenter", doIcSaving);
+document.querySelector("#icsaving").addEventListener("mouseleave", doIcSaving);
+
+document.querySelector("#ictower").addEventListener("mouseenter", doIcTower);
+document.querySelector("#ictower").addEventListener("mouseleave", doIcTower);
+
+document.querySelector("#iccare").addEventListener("mouseenter", doIcCare);
+document.querySelector("#iccare").addEventListener("mouseleave", doIcCare);
+
+document.querySelector("#icgoodybag").addEventListener("mouseenter", doIcGoody);
+document.querySelector("#icgoodybag").addEventListener("mouseleave", doIcGoody);
+
+document.querySelector("#ichouse").addEventListener("mouseenter", doIcHouse);
+document.querySelector("#ichouse").addEventListener("mouseleave", doIcHouse);
+
+document.querySelector("#icgraduate").addEventListener("mouseenter", doIcGraduate);
+document.querySelector("#icgraduate").addEventListener("mouseleave", doIcGraduate);
+
+document.querySelector("#icbusiness").addEventListener("mouseenter", doIcBusiness);
+document.querySelector("#icbusiness").addEventListener("mouseleave", doIcBusiness);
+
+document.querySelector("#icbtob").addEventListener("mouseenter", doIcBtob);
+document.querySelector("#icbtob").addEventListener("mouseleave", doIcBtob);
+
+document.querySelector("#icagriculture").addEventListener("mouseenter", doIcAgriculture);
+document.querySelector("#icagriculture").addEventListener("mouseleave", doIcAgriculture);
+
+document.querySelector("#icgear").addEventListener("mouseenter", doIcGear);
+document.querySelector("#icgear").addEventListener("mouseleave", doIcGear);
+
+document.querySelector("#icfood").addEventListener("mouseenter", doIcFood);
+document.querySelector("#icfood").addEventListener("mouseleave", doIcFood);
+
+function doIcManufacturing() {
+  icmanufacturing.reversed(!icmanufacturing.reversed());
+  manufacturing.reversed(!manufacturing.reversed());
+ 
+}
+
+function doIcConstruction() {
+    icconstruction.reversed(!construction.reversed());
+    construction.reversed(!construction.reversed());
+   
+}
+
+function doIcWholesale() {
+    icwholesale.reversed(!icwholesale.reversed());
+    wholesale.reversed(!wholesale.reversed());
+   
+}
+
+function doIcBanking() {
+    icbankinginsurance.reversed(!icbankinginsurance.reversed());
+    banking.reversed(!banking.reversed());
+   
+}
+
+function doIcPhone() {
+    icphone.reversed(!icphone.reversed());
+    telecomuni.reversed(!telecomuni.reversed());
+   
+}
+
+function doIcHealthcare() {
+    ichealthcare.reversed(!ichealthcare.reversed());
+    healthcare.reversed(!healthcare.reversed());
+   
+}
+
+function doIcSaving() {
+    icsaving.reversed(!icsaving.reversed());
+    financial.reversed(!financial.reversed());
+   
+}
+
+function doIcTower() {
+    ictower.reversed(!ictower.reversed());
+    telecom.reversed(!telecom.reversed());
+   
+}
+
+function doIcCare() {
+    iccare.reversed(!iccare.reversed());
+    hospital.reversed(!hospital.reversed());
+   
+}
+
+function doIcGoody() {
+    icgoodybag.reversed(!icgoodybag.reversed());
+    consumer.reversed(!consumer.reversed());
+   
+}
+
+function doIcHouse() {
+    ichouse.reversed(!ichouse.reversed());
+    housing.reversed(!housing.reversed());
+   
+}
+
+function doIcGraduate() {
+    icgraduate.reversed(!icgraduate.reversed());
+    education.reversed(!education.reversed());
+   
+}
+
+function doIcBusiness() {
+    icbusiness.reversed(!icbusiness.reversed());
+    btoc.reversed(!btoc.reversed());
+   
+}
+
+function doIcBtob() {
+    icbtob.reversed(!icbtob.reversed());
+    btob.reversed(!btob.reversed());
+   
+}
+
+function doIcAgriculture() {
+    icagriculture.reversed(!icagriculture.reversed());
+    agriculture.reversed(!agriculture.reversed());
+   
+}
+
+function doIcGear() {
+    icgear.reversed(!icgear.reversed());
+    resources.reversed(!resources.reversed());
+   
+}
+
+function doIcFood() {
+    icfood.reversed(!icfood.reversed());
+    beverages.reversed(!beverages.reversed());
+   
+}
+
+
